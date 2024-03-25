164c1db520f1726c24767700eb2f216904a2df9c
Quang Than <quangthan87@gmail.com>
2024-03-25 22:29:56 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./service-worker.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-pomodoro/service-worker.js', { scope: '/react-pomodoro/' })})}
